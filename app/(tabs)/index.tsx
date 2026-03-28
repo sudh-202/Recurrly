457c3dabff52ae4dd017c3f869e7958b1b32cd92
Sudhanshu <sudhanshu@Sudhanshus-MacBook-Air.local>
2026-03-29 01:00:04 +0530

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,33 +1,47 @@
-import "@/global.css"
+import "@/global.css";
 import React from "react";
 import { Text } from "react-native";
 import { Link } from "expo-router";
-import { SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 import { styled } from "nativewind";
 
 const SafeAreaView = styled(RNSafeAreaView);
- 
+
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-background p-5 ">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
-      <Link href="/onboarding" className="text-xl font-bold text-success bg-primary">
+    <SafeAreaView className="flex-1  bg-background p-5 ">
+
+      <Text className="text-7xl font-sans-extrabold ">Home</Text>
+      <Link
+        href="/onboarding"
+        className="text-xl font-sans-bold text-white px-5 py-2 my-2 bg-primary"
+      >
         Onboarding
       </Link>
-      <Link href="/(auth)/sign-in" className="text-xl font-bold text-success bg-primary">
+      <Link
+        href="/(auth)/sign-in"
+        className="text-xl font-sans-bold px-5 py-2 my-2 text-white bg-primary"
+      >
         Sign In
       </Link>
-      <Link href="/(auth)/sign-up" className="text-xl font-bold text-success bg-primary">
+      <Link
+        href="/(auth)/sign-up"
+        className="text-xl font-sans-bold px-5 py-2 my-2 text-white bg-primary"
+      >
         Sign Up
       </Link>
-      <Link href="/subscriptions/spotify" className="text-xl font-bold text-success bg-primary">
+      <Link
+        href="/subscriptions/spotify"
+        className="text-xl font-sans-bold px-5 py-2 my-2 text-white bg-primary"
+      >
         Spotify
       </Link>
-      <Link href={{pathname: "/subscriptions/[id]", params: {id: "netflix"}}} className="text-xl font-bold text-success bg-primary">
+      <Link
+        href={{ pathname: "/subscriptions/[id]", params: { id: "netflix" } }}
+        className="text-xl font-sans-bold px-5 py-2 my-2 text-white bg-primary"
+      >
         Netflix
       </Link>
-    </SafeAreaView >
+    </SafeAreaView>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,27 @@
 import { Stack } from "expo-router";
 import "@/global.css";
 import React from "react";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "sans-regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "sans-light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+    "sans-medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "sans-semibold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    "sans-bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "sans-extrabold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
